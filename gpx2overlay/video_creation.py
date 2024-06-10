c68870bae9959e1e42496d191b600ceb90ac6436
SuntzuDragon <imcb01@gmail.com>
2024-06-10 13:40:28 -0600

    Formatting

--- gpx2overlay/video_creation.py
+++ gpx2overlay/video_creation.py
@@ -1,17 +1,19 @@
 import os
 import subprocess
 
+
 def create_video(ffmpeg_executable, output_dir, image_pattern, fps, video_file):
     input_frames = os.path.join(output_dir, image_pattern)
     nvenc_check_cmd = [ffmpeg_executable, '-v', 'error', '-encoders']
-    nvenc_check_result = subprocess.run(nvenc_check_cmd, capture_output=True, text=True)
+    nvenc_check_result = subprocess.run(
+        nvenc_check_cmd, capture_output=True, text=True)
     encoder = 'h264_nvenc' if 'h264_nvenc' in nvenc_check_result.stdout else 'libx264'
-    
+
     if encoder == 'h264_nvenc':
         print('NVENC is supported. Using NVENC for hardware acceleration.')
     else:
         print('NVENC is not supported. Falling back to libx264 for software encoding.')
-    
+
     ffmpeg_cmd = [
         ffmpeg_executable,
         '-y',
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 import shutil
 from gpx2overlay import download_and_extract_ffmpeg, create_frames, create_video, parse_arguments, load_gpx
 
@@ -14,10 +13,11 @@ def main():
                 'FFmpeg is required but could not be downloaded or found.')
 
     points_df = load_gpx(args.gpx_file)
-    
+
     image_pattern = 'frame_%04d.png'
     create_frames(points_df, args.output_dir, image_pattern, (800, 800))
-    create_video(ffmpeg_executable, args.output_dir, image_pattern, args.fps, args.video_file)
+    create_video(ffmpeg_executable, args.output_dir,
+                 image_pattern, args.fps, args.video_file)
 
 
 if __name__ == "__main__":
